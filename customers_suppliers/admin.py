77cfb540018ce72142806ea2201928c5ef54fdff
Konstantin <kostyan-perm12@rambler.ru>
2024-11-26 22:29:11 +0500

    add models

--- customers_suppliers/admin.py
+++ customers_suppliers/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from customers_suppliers.models import CustomUser, Customer, Supplier
+
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ['id', 'email', 'is_customer', 'is_supplier']
+    list_filter = ['id', 'email']
+    
+    
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'phone_number']
+    list_filter = ['id']
+
+  
+@admin.register(Supplier)
+class SupplierAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'contact_person']
+    list_filter = ['id'] 
+
+
--- customers_suppliers/models.py
+++ customers_suppliers/models.py
@@ -1,3 +1,32 @@
+from django.contrib.auth.models import AbstractUser, User
 from django.db import models
 
-# Create your models here.
+
+class CustomUser (AbstractUser ):
+    email = models.EmailField(unique=True)
+    is_customer = models.BooleanField(default=False)
+    is_supplier = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return f"{self.username} ({self.email})"
+
+
+class Customer(models.Model):
+    user = models.OneToOneField(CustomUser , related_name='customer', on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.user.username} (Клиент)"
+
+
+class Supplier(models.Model):
+    user = models.OneToOneField(CustomUser , related_name='supplier', on_delete=models.CASCADE)
+    contact_person = models.CharField(max_length=50, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.user.username} (Поставщик)"
+    
+    
